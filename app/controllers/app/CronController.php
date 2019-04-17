433a64371e2efd1bcfa340b61ac178bd3efe70eb
System Administrator <root@allans-MacBook-Air.local>
2019-04-17 14:20:16 +0800

    - fixed

--- app/controllers/app/CronController.php
+++ app/controllers/app/CronController.php
@@ -551,7 +551,7 @@ class CronController extends \BaseController {
 
     public function createAutomaticDeletion( )
     {
-        $date = date('Y-m-d');
+        $date = date('Y-m-d', strtotime('2019-04-18'));
         $employee = 0;
         $dependents = 0;
         $withdraw = DB::table('customer_plan_withdraw')->where('date_withdraw', '<=', $date)->where('refund_status', 0)->get();
@@ -593,8 +593,10 @@ class CronController extends \BaseController {
 
             \PlanWithdraw::where('user_id', $user->user_id)->update(['refund_status' => 1]);
             // update customer plan draw to 1
-            if($user->keep_seat == 0) {
+            if((int)$user->keep_seat == 0) {
                 PlanHelper::updateNewCustomerPlanStatusDeleteUser($user->user_id, $refund_status);
+            } else if((int)$user->vacate_seat == 1) {
+                PlanHelper::updateCustomerPlanStatusDeleteUserVacantSeat($user->user_id);
             }
             PlanHelper::removeDependentAccounts($user->user_id, $user->date_withdraw);
             $employee++;
