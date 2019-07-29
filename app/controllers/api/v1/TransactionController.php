96d8e604b4702cdb1fd5e72cc4c71ab86b183868
System Administrator <root@allans-MacBook-Air.local>
2019-07-29 16:31:29 +0800

    - test temp enrollment

--- app/controllers/api/v1/TransactionController.php
+++ app/controllers/api/v1/TransactionController.php
@@ -1471,27 +1471,27 @@ class Api_V1_TransactionController extends \BaseController
 						    $aws_upload = true;
 						    $result = $trans_docs->saveReceipt($receipt);
 						} else {
-							// $file_name = StringHelper::get_random_password(6).' - '.$file_name;
-							// $file->move(public_path().'/temp_uploads/', $file_name);
-							// $result_doc = Queue::connection('redis_high')->push('\InNetworkFileUploadQueue', array('file' => public_path().'/temp_uploads/'.$file_name, 'transaction_id' => $transaction_id, 'user_id' => $check->UserID));
-							//   $file_address = url('temp_uploads', $parameter = array(), $secure = null).'/'.$file_name;
-       //          $result = array(
-       //            'file'      => $file_address,
-					  //      	'type'      => "image",
-					  //      	'transaction_id'    => $transaction_id,
-					  //      	'user_id'		=> $check->UserID,
-					  //      	'id'				=> rand(),
-					  //      	'created_at'	=> date('Y-m-d H:i:s'),
-					  //      	'updated_at' => date('Y-m-d H:i:s')
-       //          );
-						    $image = \Cloudinary\Uploader::upload($file->getPathName());
-						    $receipt = array(
-					       'user_id'           => $findUserID,
-					       'file'      => $image['secure_url'],
-					       'type'      => "image",
-					       'transaction_id'    => $transaction_id,
-						   );
-						    $result = $trans_docs->saveReceipt($receipt);
+							$file_name = StringHelper::get_random_password(6).' - '.$file_name;
+							$file->move(public_path().'/temp_uploads/', $file_name);
+							$result_doc = Queue::connection('redis_high')->push('\InNetworkFileUploadQueue', array('file' => public_path().'/temp_uploads/'.$file_name, 'transaction_id' => $transaction_id, 'user_id' => $check->UserID));
+							  $file_address = url('temp_uploads', $parameter = array(), $secure = null).'/'.$file_name;
+                $result = array(
+                  'file'      => $file_address,
+					       	'type'      => "image",
+					       	'transaction_id'    => $transaction_id,
+					       	'user_id'		=> $check->UserID,
+					       	'id'				=> rand(),
+					       	'created_at'	=> date('Y-m-d H:i:s'),
+					       	'updated_at' => date('Y-m-d H:i:s')
+                );
+						   //  $image = \Cloudinary\Uploader::upload($file->getPathName());
+						   //  $receipt = array(
+					    //    'user_id'           => $findUserID,
+					    //    'file'      => $image['secure_url'],
+					    //    'type'      => "image",
+					    //    'transaction_id'    => $transaction_id,
+						   // );
+						   //  $result = $trans_docs->saveReceipt($receipt);
 						}
 
 						
