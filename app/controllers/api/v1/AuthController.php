d90f66093c70ec624c66e6341f7fabb318c003dd
System Administrator <root@allans-MacBook-Air.local>
2019-09-23 17:32:51 +0800

    - fixed

--- app/controllers/api/v1/AuthController.php
+++ app/controllers/api/v1/AuthController.php
@@ -3870,7 +3870,7 @@ public function getFamilCoverageAccounts( )
                   'user_id'   => $user->UserID,
                   'name'      => ucwords($user->Name),
                   'nric'      => $user->NRIC,
-                  'dob'       => date('d/m/Y', strtotime($owner_profile->DOB)),
+                  'dob'       => date('d/m/Y', strtotime($user->DOB)),
                   'type'      => 'Owner'
               );
            } else if($user->UserType == 5 && $user->access_type == 2 || $user->UserType == 5 && $user->access_type == 3) {
