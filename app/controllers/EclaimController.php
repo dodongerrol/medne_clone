4398c4ef5c0b6e255dd995abe9bc29d6a2f9f9b1
System Administrator <root@allans-MacBook-Air.local>
2019-10-08 10:22:35 +0800

    - fixed

--- app/controllers/EclaimController.php
+++ app/controllers/EclaimController.php
@@ -23,8 +23,8 @@ class EclaimController extends \BaseController {
 	public function loginEmployee( )
 	{
 		$input = Input::all();
-    $email = (int)$input['email'];
-    $email = (string)($email);
+    $email = $input['email'];
+    // $email = (string)($email);
     $password = $input['password'];
     
 		$check = DB::table('user')
@@ -43,7 +43,7 @@ class EclaimController extends \BaseController {
     ->orWhere(function($query) use ($email, $password){
     	$email = (int)($email);
     	$query->where('UserType', 5)
-			->where('PhoneNo', $email)
+			->where('PhoneNo', (string)$email)
 		  ->where('password', md5($password))
 		  ->where('Active', 1);
     })
