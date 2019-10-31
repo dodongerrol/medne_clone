23b0b7d9bff92929c4f20a51dee06419907cb076
System Administrator <root@allans-MacBook-Air.local>
2019-10-31 14:59:53 +0800

    - fixed e-claim service lists

--- app/controllers/api/v1/AuthController.php
+++ app/controllers/api/v1/AuthController.php
@@ -4989,13 +4989,14 @@ public function getHealthLists( )
                                         ->select('health_types.name', 'health_types.health_type_id', 'health_types.created_at', 'health_types.updated_at', 'company_e_claim_service_types.active')
                                         ->get();
       if(sizeof($get_company_e_claim_services) > 0) {
-        return $get_company_e_claim_services;
+        $spending_types = $get_company_e_claim_services;
       } else { 
-        return DB::table('health_types')->where('type', $input['spending_type'])->where('active', 1)->get();
+        $spending_types = DB::table('health_types')->where('type', $input['spending_type'])->where('active', 1)->get();
       }
+    } else {
+      $spending_types = DB::table('health_types')->where('type', $input['spending_type'])->where('active', 1)->get();
     }
-
-   $spending_types = DB::table('health_types')->where('type', $input['spending_type'])->where('active', 1)->get();
+    
    $returnObject->data = $spending_types;
    return Response::json($returnObject);
  } else {
