9ec9e1ed5715bd2730375b8885327bb103733422
Noelou Nagac <noelounagac@Noelous-MacBook-Air.local>
2019-11-21 15:46:10 +0800

    add some ui in file uploaded modal

--- public/assets/hr-dashboard/process/directives/capPerVisitDirective.js
+++ public/assets/hr-dashboard/process/directives/capPerVisitDirective.js
@@ -8,13 +8,25 @@ app.directive('capPerVisitDirective', [
 			scope: true,
 			link: function link( scope, element, attributeSet ) {
 				console.log("capPerVisitDirective Runnning !");
-				
+				scope.showDataText = true;
+				scope.showInputText = false;
+
 				scope.fileUploadModal = function( emp ){
 					scope.selected_emp = emp;
 				}
 				
 				scope.closePass = function( ) {
 					$('#file_upload').modal('hide');
+				}
+
+				scope.editTableCell = function () {
+					if ( scope.showDataText == true ) {
+						scope.showDataText = false;
+						scope.showInputText = true;
+						 $("button").removeClass("save-continue-disabled");
+					} else {
+						scope.showDatatText = false;
+					}
 				}
        
         scope.onLoad = function( ){
