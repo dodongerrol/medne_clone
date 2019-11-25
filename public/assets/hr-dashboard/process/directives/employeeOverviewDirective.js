a340316c6137e424f7241befc8ed664a3b07f57c
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2019-11-25 17:40:32 +0800

    udpates

--- public/assets/hr-dashboard/process/directives/employeeOverviewDirective.js
+++ public/assets/hr-dashboard/process/directives/employeeOverviewDirective.js
@@ -166,7 +166,6 @@ app.directive("employeeOverviewDirective", [
             $('.opened-per-page-scroll').hide();
           }
           scope.searchClinics = function( search, opt ) {
-            console.log( scope.search );
             if( search != "" ){
               if( opt == 'block' ){
                 scope.block_page = 1;
@@ -418,7 +417,6 @@ app.directive("employeeOverviewDirective", [
               angular.forEach( scope.clinic_block_arr, function( value, key ){
                 if( value.selected ){
                   ctr += 1;
-                  console.log( value.selected );
                   scope.showLoading();
                   scope.updateClinics( value.ClinicID, status, region, opt );
                 }
@@ -471,7 +469,7 @@ app.directive("employeeOverviewDirective", [
                 console.log(response);
                 scope.clinic_type_open_arr = response.data;
               });
-            hrActivity.fetchClinicTypesEmp( 'block', scope.filter_regionBlocked ) 
+            hrActivity.fetchClinicTypesEmp( 'block', scope.filter_regionBlocked, scope.selectedEmployee.user_id ) 
               .then(function(response) { 
                 console.log(response);
                 scope.clinic_type_block_arr = response.data;
