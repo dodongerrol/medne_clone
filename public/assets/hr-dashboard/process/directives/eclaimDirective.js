b9f58c8c4ec29afbcb186a47108cab21e5066e61
System Administrator <root@allans-MacBook-Air.local>
2020-01-10 14:40:55 +0800

    - fixed

--- public/assets/hr-dashboard/process/directives/eclaimDirective.js
+++ public/assets/hr-dashboard/process/directives/eclaimDirective.js
@@ -356,7 +356,7 @@
 								list.approved_status = true;
 								list.approved_date = moment().format( 'DD MMMM YYYY hh:mm A' );
 								list.rejected_date = null;
-								list.claim_amount = parseFloat(list.approve_claim_amount).toFixed(2);
+								list.claim_amount = list.approve_claim_amount;
 							}
 							
 							if( response.data.status == true ){
