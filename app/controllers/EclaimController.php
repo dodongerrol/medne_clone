0d81ef666b23c130129bc55a376bd194e1280697
Allan Cheam Alzula <allan.alzula@gmail.com>
2020-03-12 16:21:36 +0800

    - fixed

--- app/controllers/EclaimController.php
+++ app/controllers/EclaimController.php
@@ -3390,7 +3390,7 @@ public function getActivityInNetworkTransactions( )
 					if((int)$trans->lite_plan_enabled == 1) {
 						if((int)$trans->half_credits == 1) {
 							$total_amount = $trans->credit_cost + $trans->consultation_fees;
-							$cash = $transation->cash_cost;
+							$cash = $trans->cash_cost;
 						} else {
 							$total_amount = $trans->procedure_cost;
 							$total_amount = $trans->procedure_cost + $trans->consultation_fees;
