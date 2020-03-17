0485b5619ad1a7c37d20936801c538568c1b9db3
Allan Cheam Alzula <allan.alzula@gmail.com>
2020-03-17 18:27:40 +0800

    - fixed issue on wellness wallet credit balance recalculation

--- app/helpers/PlanHelper.php
+++ app/helpers/PlanHelper.php
@@ -2327,11 +2327,9 @@ class PlanHelper {
 		$e_wallet = DB::table('e_wallet')->where('wallet_id', $wallet_id)->first();
 		$user = DB::table('user')->where('UserID', $user_id)->first();
 		$wallet_history = DB::table('wellness_wallet_history')
-											->join('e_wallet', 'e_wallet.wallet_id', '=', 'wellness_wallet_history.wallet_id')
-											->where('wellness_wallet_history.wallet_id', $wallet_id)
-											->where('e_wallet.UserID', $user_id)
-											->where('wellness_wallet_history.created_at',  '>=', $start)
-											->where('wellness_wallet_history.created_at',  '<=', $end)
+											->where('wallet_id', $wallet_id)
+											->where('created_at',  '>=', $start)
+											->where('created_at',  '<=', $end)
 											->get();
 
 		foreach ($wallet_history as $key => $history) {
