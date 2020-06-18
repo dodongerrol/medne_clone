9628ce473cdcba3df07f3e9111a067d2c6016959
Errol John Cañeda <erroljohncaneda@gmail.com>
2020-06-18 15:26:44 +0800

    no message

--- app/controllers/EmployeeController.php
+++ app/controllers/EmployeeController.php
@@ -3011,13 +3011,13 @@ class EmployeeController extends \BaseController {
     $email = DB::table('customer_hr_dashboard')->where('email', $input['email'])->first();
     $token = StringHelper::getToken();
 
-    if(!$email) {
-      return array('status' => FALSE, 'message' => 'Your email has not been signed up with Mednefits.');
+    if(!$email == 2) {
+      return array('status' => 2, 'message' => 'Your email has not been signed up with Mednefits.');
     }
-    if($email && (int)$email->active == 0) {
-      return array('status' => FALSE, 'message' => 'Sorry, your email has not yet been activated. Please check your inbox for your activation email.');
-    } else if($email && (int)$email->active == 1) {
-      return array('status' => TRUE, 'Activated');
+    if($email && $email->hr_activated == 1) {
+      return array('status' => 1, 'message' => 'Account Activated');
+    } else if($email && $email->hr_activated == 0) {
+      return array('status' => 0, 'message' => 'Sorry, your email has not yet been activated. Please check your inbox for your activation email.');
     }
     if($email) {
       return $token;
