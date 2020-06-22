35d12ccff3d80d19440330c4bcb20bf8e0bed570
Allan Cheam Alzula <allan.alzula@gmail.com>
2020-06-22 15:10:55 +0800

    - partial

--- app/helpers/PlanHelper.php
+++ app/helpers/PlanHelper.php
@@ -3631,29 +3631,19 @@ class PlanHelper {
 	public static function createPaymentsRefund($id, $date_refund)
 	{
 		$active_plan = DB::table('customer_active_plan')->where('customer_active_plan_id', $id)->first();
-		// $refund_count = DB::table('payment_refund')
-		// ->join('customer_active_plan', 'customer_active_plan.customer_active_plan_id', '=', 'payment_refund.customer_active_plan_id')
-		// ->join('customer_plan', 'customer_plan.customer_plan_id', '=', 'customer_active_plan.plan_id')
-		// ->join('customer_buy_start', 'customer_buy_start.customer_buy_start_id', '=', 'customer_plan.customer_buy_start_id')
-		// ->where('customer_buy_start.customer_buy_start_id', $active_plan->customer_start_buy_id)
-		// ->count();
-
 		$customer = DB::table('customer_buy_start')->where('customer_buy_start_id', $active_plan->customer_start_buy_id)->first();
-  //     // create refund payment
-		// $check = 10 + $refund_count;
-		// $temp_invoice_number = str_pad($check, 6, "0", STR_PAD_LEFT);
-		// $invoice_number = 'OMC'.$temp_invoice_number.'A';
-		// if($refund_count > 0) {
-		// 	++$invoice_number;
-		// }
 		$invoice_number = InvoiceLibrary::getInvoiceNuber('payment_refund', 4);
 
 		$data = array(
 			'customer_active_plan_id'   => $id,
 			'cancellation_number'       => $invoice_number,
 			'date_refund'               => $date_refund,
-			'currency_type'							=> $customer->currency_type
+			'currency_type'				=> $customer->currency_type
 		);
+
+		if($active_plan->account_type == "enterprise_plan")	{
+			$data['account_type'] = $active_plan->account_type;
+		}
 
 		$result = \PaymentRefund::create($data);
 		return $result->id;
