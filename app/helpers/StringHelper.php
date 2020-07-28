53e642a3a05062fdb248563e6af49e44458a406c
Allan Cheam Alzula <allan.alzula@gmail.com>
2020-07-28 17:08:33 +0800

    - partial

--- app/helpers/StringHelper.php
+++ app/helpers/StringHelper.php
@@ -1601,7 +1601,7 @@ public static function get_random_password($length)
                     {
                         if (substr($name, 0, 5) == 'HTTP_')
                         {
-                            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('-', ' ', substr($name, 5)))))] = $value;
                         }
                     }
                     return $headers;
