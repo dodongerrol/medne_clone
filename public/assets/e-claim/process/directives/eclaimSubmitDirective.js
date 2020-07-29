18048d5e0dfcff9d125f2d0a19367d9a87cc3148
Jeamar <jeamar1234@gmail.com>
2020-07-29 14:42:26 +0800

    merge conflicts

--- public/assets/e-claim/process/directives/eclaimSubmitDirective.js
+++ public/assets/e-claim/process/directives/eclaimSubmitDirective.js
@@ -465,12 +465,15 @@ app.directive('eclaimSubmitDirective', [
 							console.log(response);
 							scope.user_details = response.data.data;
 							scope.hideIntroLoader();
-							scope.getClaims( scope.eclaim.spending_type );
+							
 							// console.log(scope.user_details);
 							// scope.getCurrentActivity();
 
 							if ( scope.user_details.wellness == true && scope.user_details.currency_type == 'myr' && scope.user_details.plan_type == 'enterprise_plan' ) {
 								scope.spendingTypeOpt = 1;
+								scope.setSpendingType(1);
+							}else{
+								scope.getClaims( scope.eclaim.spending_type );
 							}
 						});
 				}
