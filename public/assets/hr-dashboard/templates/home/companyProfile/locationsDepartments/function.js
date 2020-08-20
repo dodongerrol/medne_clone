f1dbc2d6213fca573647a00a505711e3ac0f2bea
Jantinn Erezo <erezojantinn@gmail.com>
2020-08-20 19:46:09 +0800

    Feature gods view locations departments (#219)
    * Company profile - Locations and departments with expiremental function for simplier directive.
    
    * native javascript to angularjs directive conversion

--- public/assets/hr-dashboard/templates/home/companyProfile/locationsDepartments/function.js
+++ public/assets/hr-dashboard/templates/home/companyProfile/locationsDepartments/function.js
@@ -1,65 +1,60 @@
-/**
- * Contains all Locations and Departments variables and method calls
- *
- * @jquery for extra dom manipulations
- * @axios for API calls with auth token injected out of the box
- * @return {object} location & departments directive object
- */
-function locationDepartments()  {
-    return {
-        showAddLocationModal: false, // Show or Unshow Add Location Modal
-        showEditLocationModal: false,  // Show or Unshow Edit Location Modal
-        workLocationFields: {
-            location_name: null,
-            business_address: null,
-            country: null,
-            postal_code: null,
-            employees: 0
-        },
-        workLocations: [
-            {
-                id: 1,
-                location_name: '7 Temasek Boulevard',
-                business_address: '7 Temasek Boulevard ',
-                unit: '#18-02',
-                building: 'Suntec Tower One',
-                country: 'Singapore',
-                postal_code: '038987',
-                employees: 40
-            },
-            {
-                id: 2,
-                location_name: 'One North Office',
-                business_address: 'Ayer Rajah Crescent',
-                unit: '#01-1112',
-                building: 'JTC Launchpad',
-                country: 'Singapore',
-                postal_code: '695049',
-                employees: 5
+app.directive("locationsDepartmentsDirective", [
+    "$state",
+    "serverUrl",
+    "$timeout",
+    function directive($state, serverUrl, $timeout) {
+      return {
+        restrict: "A",
+        scope: true,
+        link: function link(scope, element, attributeSet) {
+            scope.states = {
+                showAddLocationModal: false,
+                showEditLocationModal: false
             }
-        ],
-        /**
-         * Populate form fields on edit button click
-         */
-        editLocation: function (location) {
-            this.workLocationFields = { ...location };
-            this.showEditLocationModal = true;
+            scope.workLocationFields = {
+                location_name: null,
+                business_address: null,
+                country: null,
+                postal_code: null,
+                employees: 0
+            }
+            scope.workLocations = [
+                {
+                    id: 1,
+                    location_name: '7 Temasek Boulevard',
+                    business_address: '7 Temasek Boulevard ',
+                    unit: '#18-02',
+                    building: 'Suntec Tower One',
+                    country: 'Singapore',
+                    postal_code: '038987',
+                    employees: 40
+                },
+                {
+                    id: 2,
+                    location_name: 'One North Office',
+                    business_address: 'Ayer Rajah Crescent',
+                    unit: '#01-1112',
+                    building: 'JTC Launchpad',
+                    country: 'Singapore',
+                    postal_code: '695049',
+                    employees: 5
+                }
+            ]
+            scope.addLocation = () => {
+                scope.showAddLocationModal = !scope.showAddLocationModal;
+            }
+            scope.editLocation = (location) => {
+                scope.workLocationFields = { ...location };
+                scope.showEditLocationModal = true;
+            }
+            scope.saveLocation = () => {
+
+            }
+            scope.onLoad = async function () {
+                console.log('Loaded!')
+            };
+            scope.onLoad();
         },
-        /**
-         * Do a post request using axios instead of $http
-         */
-        removeLocation: async function (location)  {
-            // Do an axios POST method
-            // $http.post(url, params)
-            await axios.post(``);
-        },
-         /**
-         * Do a post request using axios instead of $http
-         */
-        updateLocation: async function () {
-            // Do an axios POST method
-            // $http.post(url, params)
-            await axios.post(``);
-        }
-    }
-}
+      };
+    },
+]);
