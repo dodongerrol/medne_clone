345b6dad7fe9679faa22526b5dd24a60fc730dc8
Errol John Cañeda <erroljohncaneda@gmail.com>
2020-08-24 15:05:41 +0800

    no message

--- app/controllers/SpendingInvoiceController.php
+++ app/controllers/SpendingInvoiceController.php
@@ -56,7 +56,11 @@ class SpendingInvoiceController extends \BaseController {
             return array('status' => FALSE, 'message' => 'Enterprise account does not require spending transaction invoice.');
         }
 
-        $check_company_transactions = SpendingInvoiceLibrary::checkCompanyTransactions($result->customer_buy_start_id, $start, $end, "post_paid");
+        if($plan->account_type == "enterprise_plan")  {
+            return array('status' => FALSE, 'message' => 'Enterprise account does not require spending transaction invoice.');
+        }
+
+        $check_company_transactions = SpendingInvoiceLibrary::checkCompanyTransactions($result->customer_buy_start_id, $start, $end);
         if(!$check_company_transactions) {
             return array('status' => FALSE, 'message' => 'No Transactions for this Month.');
         }
