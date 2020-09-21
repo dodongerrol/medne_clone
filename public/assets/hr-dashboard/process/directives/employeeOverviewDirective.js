a17b9046344db74fe02da13bee50592577a8a040
noelou <34088074+noelou@users.noreply.github.com>
2020-09-21 13:07:36 +0800

    rl-my-login-feature (#311)
    * rl-my-login-feature
    
    * rl-my-login-feature

--- public/assets/hr-dashboard/process/directives/employeeOverviewDirective.js
+++ public/assets/hr-dashboard/process/directives/employeeOverviewDirective.js
@@ -2530,6 +2530,7 @@ app.directive("employeeOverviewDirective", [
                   user_id: data.user_id,
                   bank_name: data.bank_name,
                   emp_id: data.employee_id,
+                  passport: data.passport,
                 };
                 console.log(update_data);
                 dependentsSettings.updateEmployee(update_data)
