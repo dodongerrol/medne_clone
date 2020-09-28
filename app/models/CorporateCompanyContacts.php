073bc49cd9409f2d6785bbc4b17ca53b0fc38cbb
Errol John Cañeda <erroljohncaneda@gmail.com>
2020-09-28 16:07:38 +0800

    update and get company contacts

--- app/models/CorporateCompanyContacts.php
+++ app/models/CorporateCompanyContacts.php
@@ -6,19 +6,19 @@ class CorporateCompanyContacts extends Eloquent
     protected $table = 'company_contacts';
     protected $guarded = ['medi_company_contact_id'];
 
-    public function getHrLocations($id)
+    public function getCompanyContacts($id)
     {
-        return CorporateHrLocation::where('customer_id', $id)->orderBy('created_at','desc')->first();
+        return CorporateCompanyContacts::where('customer_id', $id)->orderBy('created_at','desc')->first();
     }
 
-    public function insertCorporateHrLocations($data)
+    public function insertCompanyContacts($data)
     {
-        return CorporateHrLocation::create($data);
+        return CorporateCompanyContacts::create($data);
     }
 
-    public function updateCorporateHrLocations($id, $data)
+    public function updateCompanyContacts($id, $data)
     {
-        return CorporateHrLocation::where('medi_company_contact_id', $id)->update($data);
+        return CorporateCompanyContacts::where('medi_company_contact_id', $id)->update($data);
     }
     
 }
