30caacb2c6646356af782c621dd96290e405849f
Errol John Cañeda <erroljohncaneda@gmail.com>
2020-09-29 14:19:46 +0800

    no message

--- app/controllers/BenefitsDashboardController.php
+++ app/controllers/BenefitsDashboardController.php
@@ -4937,52 +4937,35 @@ class BenefitsDashboardController extends \BaseController {
 
 	public function updateBillingContact( )
 	{
-		$input = Input::all();
-		$result = self::checkSession();
-		// get admin session from mednefits admin login
 		$admin_id = Session::get('admin-session-id');
-		$hr_id = $result->hr_dashboard_id;
+		$hr_data = StringHelper::getJwtHrSession();
+		$id = $hr_data->customer_buy_start_id;
+		$input = Input::all();
+
+		$check = DB::table('customer_business_contact')->where('customer_business_contact_id', $id)->first();
+		
+		$business_contact = new CorporateBusinessContact;
 
 		// customer_billing_contact_id
-		$details = array(
-			'first_name'		=> $input['first_name'],
-			'last_name'			=> $input['last_name'],
-			'billing_email'		=> $input['work_email'],
-			'updated_at'		=> date('Y-m-d H:i:s')
+		$data = array(
+			'first_name'					=> !empty($input['first_name']) ? $input['first_name'] : $check->first_name,
+			'work_email'					=> !empty($input['work_email']) ? $input['work_email'] : $check->work_email,
+			'updated_at'					=> date('Y-m-d H:i:s')
 		);
 
-		$result = DB::table('customer_billing_contact')
-		->where('customer_billing_contact_id', $input['customer_billing_contact_id'])
-		->update($details);
+		$result = $business_contact
+		->updateBusinessContact($input['customer_business_contact_id'], $data);
 
-		if($result) {
-			if($admin_id) {
-				$admin_logs = array(
-					'admin_id'  => $admin_id,
-					'admin_type' => 'mednefits',
-					'type'      => 'admin_hr_updated_company_billing_contact',
-					'data'      => SystemLogLibrary::serializeData($input)
-				);
-				SystemLogLibrary::createAdminLog($admin_logs);
-			} else {
-				$admin_logs = array(
-					'admin_id'  => $hr_id,
-					'admin_type' => 'hr',
-					'type'      => 'admin_hr_updated_company_billing_contact',
-					'data'      => SystemLogLibrary::serializeData($input)
-				);
-				SystemLogLibrary::createAdminLog($admin_logs);
-			}
-			return array(
-				'status'	=> TRUE,
-				'message'	=> 'Success.'
-			);
-		}
-
-		return array(
-			'status'	=> FALSE,
-			'message'	=> 'Failed.'
+		$info = DB::table('customer_buy_start')->where('customer_buy_start_id', $id)->first();
+		$customer = array(
+			'currency_type'					=> !empty($input['currency_type']) ? $input['currency_type'] : $info->currency_type,
 		);
+
+		$account = DB::table('customer_buy_start')
+		->where('customer_buy_start_id', $id)
+		->update($customer);
+
+		return array('status' => TRUE, 'message'	=> 'successfully updated business contact.');
 	}
 
 	public function benefitsTransactions( )
