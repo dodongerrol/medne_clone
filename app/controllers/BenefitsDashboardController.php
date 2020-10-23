894b698538a01f4ddae884ab36db483cce6d8669
Errol John Cañeda <erroljohncaneda@gmail.com>
2020-10-23 15:36:46 +0800

    no message

--- app/controllers/BenefitsDashboardController.php
+++ app/controllers/BenefitsDashboardController.php
@@ -18114,6 +18114,18 @@ public function createHrLocation ()
 		$employee_id = $input['employee_id'] ?? null;
 		$location_id = $input['location_id'] ?? null;
 
+		if($search) {
+			$users = DB::table('user')
+			->join('corporate_members', 'corporate_members.user_id', '=', 'user.UserID')
+			->where('corporate_members.corporate_id', $account_link->corporate_id)
+			->where('user.Name', 'like', '%'.$search.'%')
+			->where('user.member_activated', 1)
+			->select('user.UserID', 'user.Name')
+			->get();
+			
+			return $users;
+		}
+
 		if((int)$is_mednefits_employee == 1) {
 			$employee = DB::table('user')
 				->join('corporate_members', 'corporate_members.user_id', '=', 'user.UserID')
@@ -18456,8 +18468,27 @@ public function createHrLocation ()
         $result = StringHelper::getJwtHrSession();
 		$customer_id = $result->customer_buy_start_id;
 
+
+		$container = array();
+		$customers = array();
+		$today = date_create(\PlanHelper::endDate(date('Y-m-d')));
+		// $today = new \DateTime(\PlanHelper::endDate(date('Y-m-d')));
+
+
+		$employee_id = $input['employee_id'];
 		$search = !empty($input['search']) ? $input['search'] : null;
 
-		
+		$account_link = DB::table('customer_link_customer_buy')->where('customer_buy_start_id', $customer_id)->first();
+
+		$data = [
+			'message'	=> null,
+			'status'	=> true 
+		];
+
+		$employee = DB::table('user')
+				->join('corporate_members', 'corporate_members.user_id', '=', 'user.UserID')
+				->where('corporate_members.corporate_id', $account_link->corporate_id)
+				->where('user.UserID', $employee_id)
+				->first();
 	}
 }
