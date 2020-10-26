719b10a78f489d6675e1a65539dda1dcb8c25cc5
Jeamar <jeamar1234@gmail.com>
2020-10-26 10:19:39 +0800

    reset all functionalities

--- public/assets/hr-dashboard/templates/bulkUpdateMemberDetails/updateOptions/function.js
+++ public/assets/hr-dashboard/templates/bulkUpdateMemberDetails/updateOptions/function.js
@@ -63,59 +63,7 @@ app.directive("updateOptionDirective", [
             selected: false
           },
         ];
-        scope.selectIsDisabled = true;
-        scope.selectAll = ()=>{
-          scope.details.forEach( item => {
-            if (item.selected == false) {
-              console.log(scope.selected_account_types)
-              scope.selected_account_types.push(item);
-              item.selected = true;
-              scope.isDisabled = false;
-            }
-          });
-        }
-        scope.deselectAll = ()=>{
-          scope.details.forEach( item => {
-            if (item.selected == true && item.name != 'Full Name') {
-                item.selected = false;
-                scope.isDisabled = true;
-                scope.selected_account_types = [];
-            }
-          });
-        }
-
-        scope.check = (list)=>{
-        if (list.selected == true) {
-          scope.isDisabled = false;
-          scope.selected_account_types.push(list.name);
-        }else if(list.selected == false){
-          scope.selected_account_types.splice(list.name, 1);
-        }
-        if(scope.selected_account_types.length == 0){
-          scope.selected_account_types = [];
-          scope.isDisabled = true;
-        }
-        }
-
-        scope.checkCheckbox = () =>{
-         if (scope.account_types.employee == false && scope.account_types.dependent == false) {
-          scope.isDisabled = true;
-          scope.selected_account_types = [];
-          scope.details.forEach(item => {
-            item.selected = false;
-          });
-         }
-         else{
-          scope.details.forEach(item => {
-           if (item.name == "Full Name") {
-              item.selected = true;
-           }
-          });
-          // scope.isDisabled = true;
-         }
-        }
-
-
+        
         scope.nextBtn = function(){
           $state.go('bulk-update-member-details.excel-import');
         }
