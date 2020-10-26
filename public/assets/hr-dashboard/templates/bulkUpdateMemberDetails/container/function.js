1bda3f155a4b460e7a667024c9863a0ed9aa3e50
Michael Ligdo <mikeligdo@gmail.com>
2020-10-26 10:14:21 +0800

    rl-bulk-update-mike: bulk update

--- public/assets/hr-dashboard/templates/bulkUpdateMemberDetails/container/function.js
+++ public/assets/hr-dashboard/templates/bulkUpdateMemberDetails/container/function.js
@@ -13,26 +13,6 @@ app.directive("bulkUpdateMemberDetailsDirective", [
       link: function link(scope, element, attributeSet) {
         console.log("bulkUpdateMemberDetailsDirective Runnning !");
 
-        scope.selected_account_types = [];
-        scope.account_types = [
-          'Employee',
-          'Dependent'
-        ];
-        scope.details = [
-          'Full Name',
-          'Mobile Number',
-          'Postal Code',
-          'Locations & Department',
-          'Work Email',
-          'Date of Birth',
-          'For Communication',
-          'Bank Name & Bank Account Number',
-          'Employee ID',
-          'Benefits Start Date',
-          'Relationship'
-        ];
-
-
         scope.onLoad  = function(){
 
         }
--- public/assets/hr-dashboard/templates/bulkUpdateMemberDetails/updateOptions/function.js
+++ public/assets/hr-dashboard/templates/bulkUpdateMemberDetails/updateOptions/function.js
@@ -11,6 +11,110 @@ app.directive("updateOptionDirective", [
       scope: true,
       link: function link(scope, element, attributeSet) {
         console.log("updateOptionDirective Runnning !");
+        scope.isDisabled = true;
+        scope.selected_account_types = [];
+        scope.account_types = {
+          employee: true,
+          dependent: true,
+        }
+        scope.details = [
+          {
+            name: 'Full Name',
+            selected: true
+          },
+          {
+            name: 'Mobile Number',
+            selected: false
+          },
+          {
+            name: 'Postal Code',
+            selected: false
+          },
+          {
+            name: 'Locations & Department',
+            selected: false
+          },
+          {
+            name: 'Work Email',
+            selected: false
+          },
+          {
+            name: 'Date of Birth',
+            selected: false
+          },
+          {
+            name: 'For Communication',
+            selected: false
+          },
+          {
+            name: 'Bank Name & Bank Account Number',
+            selected: false
+          },
+          {
+            name: 'Employee ID',
+            selected: false
+          },
+          {
+            name: 'Benefits Start Date',
+            selected: false
+          },
+          {
+            name: 'Relationship',
+            selected: false
+          },
+        ];
+        scope.selectIsDisabled = true;
+        scope.selectAll = ()=>{
+          scope.details.forEach( item => {
+            if (item.selected == false) {
+              console.log(scope.selected_account_types)
+              scope.selected_account_types.push(item);
+              item.selected = true;
+              scope.isDisabled = false;
+            }
+          });
+        }
+        scope.deselectAll = ()=>{
+          scope.details.forEach( item => {
+            if (item.selected == true && item.name != 'Full Name') {
+                item.selected = false;
+                scope.isDisabled = true;
+                scope.selected_account_types = [];
+            }
+          });
+        }
+
+        scope.check = (list)=>{
+        if (list.selected == true) {
+          scope.isDisabled = false;
+          scope.selected_account_types.push(list.name);
+        }else if(list.selected == false){
+          scope.selected_account_types.splice(list.name, 1);
+        }
+        if(scope.selected_account_types.length == 0){
+          scope.selected_account_types = [];
+          scope.isDisabled = true;
+        }
+        }
+
+        scope.checkCheckbox = () =>{
+         if (scope.account_types.employee == false && scope.account_types.dependent == false) {
+          scope.isDisabled = true;
+          scope.selected_account_types = [];
+          scope.details.forEach(item => {
+            item.selected = false;
+          });
+         }
+         else{
+          scope.details.forEach(item => {
+           if (item.name == "Full Name") {
+              item.selected = true;
+           }
+          });
+          // scope.isDisabled = true;
+         }
+        }
+
 
         scope.nextBtn = function(){
           $state.go('bulk-update-member-details.excel-import');
