e6c4aa3385a3b1aa6c5cf3159135a3e836606965
Jantinn Erezo <erezojantinn@gmail.com>
2020-11-11 18:07:34 +0800

    Added job queue processing

--- app/helpers.php
+++ app/helpers.php
@@ -93,3 +93,25 @@ if (!function_exists('sum')) {
         return array_sum($values);
     }
 }
+
+if (!function_exists('isEqual')) {
+    function isEqual($value1, $value2, $strict = false): bool
+    {
+        if ($strict) {
+            return $value1 === $value2;
+        }
+
+        return $value1 == $value2;
+    }
+}
+
+if (!function_exists('isNotEqual')) {
+    function isNotEqual($value1, $value2, $strict = false): bool
+    {
+        if ($strict) {
+            return $value1 !== $value2;
+        }
+
+        return $value1 != $value2;
+    }
+}
