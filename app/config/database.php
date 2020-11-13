c1b931cc9d685bb49361bcf6a69aa24d4172f0ec
Allan Cheam Alzula <allan.alzula@gmail.com>
2020-11-13 11:56:03 +0800

    db driver test

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host' => 'localhost',
+			'host' => '127.0.0.1',
 			'database'  => 'medicloud_v3',
 			'username'  => 'root',
 			'password'  => '',
