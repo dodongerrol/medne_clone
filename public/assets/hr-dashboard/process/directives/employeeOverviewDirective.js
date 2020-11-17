69ed769506bb8194cf1cb0ec615efc234755d642
Jeamar <jeamar1234@gmail.com>
2020-11-17 14:38:25 +0800

    hotfix-#g0vwab (#590)

--- public/assets/hr-dashboard/process/directives/employeeOverviewDirective.js
+++ public/assets/hr-dashboard/process/directives/employeeOverviewDirective.js
@@ -2955,7 +2955,8 @@ app.directive("employeeOverviewDirective", [
             (scope.spending_account_status.account_type == 'lite_plan' && (scope.spending_account_status.wellness_enabled)) ||
             (scope.spending_account_status.account_type == 'enterprise_plan' && scope.spending_account_status.currency_type == 'myr' && scope.spending_account_status.wellness_enabled) || 
             (scope.spending_account_status.account_type == 'enterprise_plan' && scope.spending_account_status.currency_type == 'sgd' && (scope.spending_account_status.wellness_enabled)) ||
-            (scope.spending_account_status.account_type == 'out_of_pocket' && (scope.spending_account_status.wellness_enabled))
+            (scope.spending_account_status.account_type == 'out_of_pocket' && (scope.spending_account_status.wellness_enabled)) || 
+            (scope.spendingPlan_status.account_type != 'lite_plan' && scope.spendingPlan_status.account_type != 'enterprise_plan' && scope.spendingPlan_status.account_type != 'out_of_pocket' && (scope.spendingPlan_status.wellness_enabled))
           ){
             scope.isWellnessShow = true;
           }
