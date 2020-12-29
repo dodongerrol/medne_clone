2009d3b61434d6648a787f541e77dc2a5801f39c
neophytes08 <allan.alzula@gmail.com>
2020-12-29 15:40:09 +0800

    - db

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'driver'    => 'mysql',
 			'read' 		=> array( 'host' => '127.0.0.1', ),
 			'write' 		=> array( 'host' => '127.0.0.1', ),
-			'database'  => 'medicloud_v3_live',
+			'database'  => 'medicloud_v3',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
