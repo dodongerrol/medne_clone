5fd76c42f73c89ce0cf4908d7eefbc37187a7bae
neophytes08 <allan.alzula@gmail.com>
2021-01-13 14:57:43 +0800

    - fixed

--- app/controllers/EclaimController.php
+++ app/controllers/EclaimController.php
@@ -1320,7 +1320,7 @@ class EclaimController extends \BaseController {
 				if($consultation_credits && $trans->credit_cost > 0)	{
 					$transaction_type = "credits";
 					$payment_type = "Mednefits Credits";
-				} else {
+				} else if($consultation_credits && $trans->credit_cost > 0 && $trans->cash_cost > 0){
 					$transaction_type = "cash";
 					$payment_type = "Mednefits Credits + Cash";
 				}
