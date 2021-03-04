56e98393fcfa5e0a06dcb26f9f7be6a2b8f793a0
neophytes08 <allan.alzula@gmail.com>
2021-03-04 14:23:26 +0800

    - fixed

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -2516,14 +2516,14 @@ class TransactionController extends BaseController {
 		->where(function($query) use ($clinic_id, $start, $end){
 			$query->where('transaction_history.ClinicID', $clinic_id)
 			->where('transaction_history.paid', 1)
-			// ->where('transaction_history.procedure_cost', ">=", 0)
+			->where('transaction_history.procedure_cost', ">=", 0)
 			->where('transaction_history.claim_date', '>=', $start)
 			->where('transaction_history.claim_date', '<=', $end);
 		})
 		->orWhere(function($query) use ($clinic_id, $start, $end){
 			$query->where('transaction_history.ClinicID', $clinic_id)
 			->where('transaction_history.paid', 1)
-			// ->where('transaction_history.procedure_cost', ">=", 0)
+			->where('transaction_history.procedure_cost', ">=", 0)
 			->where('transaction_history.created_at', '>=', $start)
 			->where('transaction_history.created_at', '<=', $end);
 		})
